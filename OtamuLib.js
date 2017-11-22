ea20349e3965e864b649a7075828d67842026bca
Otamusan <tamumu.naka@gmail.com>
2017-11-22 23:42:42 +0900

    test
    test

---
+++ OtamuLib.js
@@ -0,0 +1,16 @@
+function fac(k) {
+	var j = 1;
+	for (var i = 1; i <= k; i++) {
+		j *= i;
+	}
+	return j;
+}
+
+function per(k, i) {
+	return fac(k) / fac(k - i);
+}
+
+function com(k, i) {
+	return per(k, i) / fac(i);
+}
+//test
