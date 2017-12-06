178196df2631d0c03aa9540ddeedf6d3febc17d3
Otamusan <tamumu.naka@gmail.com>
2017-12-06 21:58:38 +0900

    test

--- OtamuLib.js
+++ OtamuLib.js
@@ -1,15 +1,17 @@
-function fac(k) {
-	var j = 1;
-	for (var i = 1; i <= k; i++) {
-		j *= i;
-	}
-	return j;
-}
+module.exports={
+	fac(k) {
+		var j = 1;
+		for (var i = 1; i <= k; i++) {
+			j *= i;
+		}
+		return j;
+	},
 
-function per(k, i) {
-	return fac(k) / fac(k - i);
-}
-//test
-function com(k, i) {
-	return per(k, i) / fac(i);
+	per(k, i) {
+		return fac(k) / fac(k - i);
+	},
+
+	com(k, i) {
+		return per(k, i) / fac(i);
+	}
 }
---
+++ test.js
@@ -0,0 +1,3 @@
+var util = require("./OtamuLib.js");
+
+console.log(util.fac(5))
